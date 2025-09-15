791d31d2cd074776902c034c62421afe9633c530
Imasha <imashaudayanganie2020@gmail.com>
2025-09-15 13:18:03 +0530

    created Add Order Form

--- CoffeeShopBilling/AddOrdersForm.Designer.cs
+++ CoffeeShopBilling/AddOrdersForm.Designer.cs
@@ -30,6 +30,23 @@
         {
             this.addOrderBtn2 = new System.Windows.Forms.Button();
             this.backHomeBtn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CustomNameTxtBox = new System.Windows.Forms.TextBox();
+            this.OrderDate = new System.Windows.Forms.DateTimePicker();
+            this.EspressoRadio = new System.Windows.Forms.RadioButton();
+            this.LatteRadio = new System.Windows.Forms.RadioButton();
+            this.CappuccionRadio = new System.Windows.Forms.RadioButton();
+            this.Qauntity = new System.Windows.Forms.NumericUpDown();
+            this.SugarChk = new System.Windows.Forms.CheckBox();
+            this.MilkChk = new System.Windows.Forms.CheckBox();
+            this.WhippedChk = new System.Windows.Forms.CheckBox();
+            this.SyrupChk = new System.Windows.Forms.CheckBox();
+            this.ExtraShotChk = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Qauntity)).BeginInit();
             this.SuspendLayout();
             // 
             // addOrderBtn2
@@ -51,16 +68,197 @@
             this.backHomeBtn1.UseVisualStyleBackColor = true;
             this.backHomeBtn1.Click += new System.EventHandler(this.backHomeBtn1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Order Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CoffeeType";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Qauntity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(54, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Add - Ons";
+            // 
+            // CustomNameTxtBox
+            // 
+            this.CustomNameTxtBox.Location = new System.Drawing.Point(231, 44);
+            this.CustomNameTxtBox.Name = "CustomNameTxtBox";
+            this.CustomNameTxtBox.Size = new System.Drawing.Size(340, 22);
+            this.CustomNameTxtBox.TabIndex = 7;
+            // 
+            // OrderDate
+            // 
+            this.OrderDate.Location = new System.Drawing.Point(231, 102);
+            this.OrderDate.Name = "OrderDate";
+            this.OrderDate.Size = new System.Drawing.Size(340, 22);
+            this.OrderDate.TabIndex = 8;
+            // 
+            // EspressoRadio
+            // 
+            this.EspressoRadio.AutoSize = true;
+            this.EspressoRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EspressoRadio.Location = new System.Drawing.Point(231, 156);
+            this.EspressoRadio.Name = "EspressoRadio";
+            this.EspressoRadio.Size = new System.Drawing.Size(101, 24);
+            this.EspressoRadio.TabIndex = 9;
+            this.EspressoRadio.TabStop = true;
+            this.EspressoRadio.Text = "Espresso";
+            this.EspressoRadio.UseVisualStyleBackColor = true;
+            // 
+            // LatteRadio
+            // 
+            this.LatteRadio.AutoSize = true;
+            this.LatteRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LatteRadio.Location = new System.Drawing.Point(352, 156);
+            this.LatteRadio.Name = "LatteRadio";
+            this.LatteRadio.Size = new System.Drawing.Size(68, 24);
+            this.LatteRadio.TabIndex = 10;
+            this.LatteRadio.TabStop = true;
+            this.LatteRadio.Text = "Latte";
+            this.LatteRadio.UseVisualStyleBackColor = true;
+            this.LatteRadio.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // CappuccionRadio
+            // 
+            this.CappuccionRadio.AutoSize = true;
+            this.CappuccionRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CappuccionRadio.Location = new System.Drawing.Point(453, 154);
+            this.CappuccionRadio.Name = "CappuccionRadio";
+            this.CappuccionRadio.Size = new System.Drawing.Size(118, 24);
+            this.CappuccionRadio.TabIndex = 11;
+            this.CappuccionRadio.TabStop = true;
+            this.CappuccionRadio.Text = "Cappuccino";
+            this.CappuccionRadio.UseVisualStyleBackColor = true;
+            // 
+            // Qauntity
+            // 
+            this.Qauntity.Location = new System.Drawing.Point(231, 217);
+            this.Qauntity.Name = "Qauntity";
+            this.Qauntity.Size = new System.Drawing.Size(283, 22);
+            this.Qauntity.TabIndex = 12;
+            // 
+            // SugarChk
+            // 
+            this.SugarChk.AutoSize = true;
+            this.SugarChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SugarChk.Location = new System.Drawing.Point(238, 275);
+            this.SugarChk.Name = "SugarChk";
+            this.SugarChk.Size = new System.Drawing.Size(75, 24);
+            this.SugarChk.TabIndex = 13;
+            this.SugarChk.Text = "Sugar";
+            this.SugarChk.UseVisualStyleBackColor = true;
+            // 
+            // MilkChk
+            // 
+            this.MilkChk.AutoSize = true;
+            this.MilkChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MilkChk.Location = new System.Drawing.Point(382, 275);
+            this.MilkChk.Name = "MilkChk";
+            this.MilkChk.Size = new System.Drawing.Size(61, 24);
+            this.MilkChk.TabIndex = 14;
+            this.MilkChk.Text = "Milk";
+            this.MilkChk.UseVisualStyleBackColor = true;
+            // 
+            // WhippedChk
+            // 
+            this.WhippedChk.AutoSize = true;
+            this.WhippedChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WhippedChk.Location = new System.Drawing.Point(238, 383);
+            this.WhippedChk.Name = "WhippedChk";
+            this.WhippedChk.Size = new System.Drawing.Size(152, 24);
+            this.WhippedChk.TabIndex = 15;
+            this.WhippedChk.Text = "Whipped-Cream";
+            this.WhippedChk.UseVisualStyleBackColor = true;
+            // 
+            // SyrupChk
+            // 
+            this.SyrupChk.AutoSize = true;
+            this.SyrupChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SyrupChk.Location = new System.Drawing.Point(238, 329);
+            this.SyrupChk.Name = "SyrupChk";
+            this.SyrupChk.Size = new System.Drawing.Size(74, 24);
+            this.SyrupChk.TabIndex = 16;
+            this.SyrupChk.Text = "Syrup";
+            this.SyrupChk.UseVisualStyleBackColor = true;
+            // 
+            // ExtraShotChk
+            // 
+            this.ExtraShotChk.AutoSize = true;
+            this.ExtraShotChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExtraShotChk.Location = new System.Drawing.Point(382, 329);
+            this.ExtraShotChk.Name = "ExtraShotChk";
+            this.ExtraShotChk.Size = new System.Drawing.Size(110, 24);
+            this.ExtraShotChk.TabIndex = 17;
+            this.ExtraShotChk.Text = "Extra-Shot";
+            this.ExtraShotChk.UseVisualStyleBackColor = true;
+            // 
             // AddOrdersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(709, 687);
+            this.Controls.Add(this.ExtraShotChk);
+            this.Controls.Add(this.SyrupChk);
+            this.Controls.Add(this.WhippedChk);
+            this.Controls.Add(this.MilkChk);
+            this.Controls.Add(this.SugarChk);
+            this.Controls.Add(this.Qauntity);
+            this.Controls.Add(this.CappuccionRadio);
+            this.Controls.Add(this.LatteRadio);
+            this.Controls.Add(this.EspressoRadio);
+            this.Controls.Add(this.OrderDate);
+            this.Controls.Add(this.CustomNameTxtBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.backHomeBtn1);
             this.Controls.Add(this.addOrderBtn2);
             this.Name = "AddOrdersForm";
             this.Text = "AddOrdersForm";
+            ((System.ComponentModel.ISupportInitialize)(this.Qauntity)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +266,21 @@
 
         private System.Windows.Forms.Button addOrderBtn2;
         private System.Windows.Forms.Button backHomeBtn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CustomNameTxtBox;
+        private System.Windows.Forms.DateTimePicker OrderDate;
+        private System.Windows.Forms.RadioButton EspressoRadio;
+        private System.Windows.Forms.RadioButton LatteRadio;
+        private System.Windows.Forms.RadioButton CappuccionRadio;
+        private System.Windows.Forms.NumericUpDown Qauntity;
+        private System.Windows.Forms.CheckBox SugarChk;
+        private System.Windows.Forms.CheckBox MilkChk;
+        private System.Windows.Forms.CheckBox WhippedChk;
+        private System.Windows.Forms.CheckBox SyrupChk;
+        private System.Windows.Forms.CheckBox ExtraShotChk;
     }
 }
