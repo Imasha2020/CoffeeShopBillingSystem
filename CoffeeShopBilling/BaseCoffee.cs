86a4e128de49bd51efd1d12d2930b6d11dc23d45
Imasha <imashaudayanganie2020@gmail.com>
2025-09-15 12:26:02 +0530

    base Coffee and base decorator created

---
+++ CoffeeShopBilling/BaseCoffee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopBilling
+{
+    public class BaseCoffee : ICoffee
+    {
+        private double baseCost;
+        private string baseDescription;
+        private int qauntity;
+
+        public BaseCoffee(int qauntity, string CoffeeType) {
+            this.qauntity = qauntity;
+            this.baseDescription = "";
+            if (CoffeeType == "espresso")
+                baseCost = 200;
+            else if (CoffeeType == "latte")
+                baseCost = 300;
+            else if (CoffeeType == "cappuccino")
+                baseCost = 350;
+        }
+
+        public double GetCost()
+        { 
+            return baseCost * qauntity;
+        }
+
+        public string GetDescription()
+        {
+            return baseDescription;
+        }
+
+    }
+}
---
+++ CoffeeShopBilling/ICoffee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopBilling
+{
+    public interface ICoffee
+    {
+        double GetCost();
+        string GetDescription();
+    }
+}
